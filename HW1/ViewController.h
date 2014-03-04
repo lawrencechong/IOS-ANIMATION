ecc6f2058454359b0b47d4f4a35850373dcbc7cf
Lawrence Chong <lawrencechong@Lawrences-MacBook-Air.local>
2014-03-03 20:12:20 -0500

    Initial Commit

---
+++ HW1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HW1
+//
+//  Created by Lawrence Chong on 3/3/14.
+//  Copyright (c) 2014 Lawrence Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HW1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HW1
+//
+//  Created by Lawrence Chong on 3/3/14.
+//  Copyright (c) 2014 Lawrence Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
